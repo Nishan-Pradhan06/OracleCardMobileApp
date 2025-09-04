8e683e98e0938534a725b17904de3a2c02ead529
nishan <nishanpradhan500@gmail.com>
2025-09-04 07:14:51 +0545

    setup app route

---
+++ lib/router/app_router.dart
@@ -0,0 +1,9 @@
+
+import 'package:go_router/go_router.dart';
+import 'package:oracle_card_app/common/extension.dart';
+import 'app_routes_names.dart';
+
+final appRoute = GoRouter(
+  initialLocation: AppRoutesName.splashScreen.path,
+  routes: [],
+);
---
+++ lib/router/app_routes_names.dart
@@ -0,0 +1,29 @@
+class AppRoutesName {
+  //#---------------GLOBAL ROUTES NANE--------------##
+  static const String splashScreen = 'splashScreen';
+  static const String onBoarding = 'onBoarding';
+  static const String loginScreen = 'loginScreen';
+  static const String registerScreen = 'registerScreen';
+  static const String forgetPassword = 'forgetPassword';
+  static const String checkoutScreen = 'checkoutScreen';
+
+  //#---------------USER ROUTES NAME--------------##
+  static const String bottomNavBar = 'bottomNavBar';
+  static const String customerHome = 'customerHome';
+  static const String themeSelectionScreen = 'themeSelectionScreen';
+  static const String helpScreen = 'helpScreen';
+  static const String notificationScreen = 'notificationScreen';
+  static const String productDetailsScreen = 'productDetailsScreen';
+  static const String wishListScreen = 'wishListScreen';
+  static const String allOrders = 'allOrders';
+
+  //#--------------VENDOR ROUTES NAME-------------##
+  static const String vendorBottomNavBar = 'vendorBottomNavBar';
+  static const String vendorDashboard = 'vendorDashboard';
+  static const String vendorProducts = 'vendorProducts';
+  static const String vendorOrders = 'vendorOrders';
+  static const String vendorAnalytics = 'vendorAnalytics';
+  static const String vendorCustomers = 'vendorCustomers';
+  static const String vendorSettings = 'vendorSettings';
+  static const String updateVendorStatus = 'updateVendorStatus';
+}
