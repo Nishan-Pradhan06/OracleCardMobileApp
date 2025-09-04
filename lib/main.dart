7bbccda2b98ffa8b37876ed7fb0fd34773c3f34e
nishan <nishanpradhan500@gmail.com>
2025-09-04 07:18:28 +0545

    load .env setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:oracle_card_app/router/app_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
+
+  
   runApp(const OracelCard());
 }
 
