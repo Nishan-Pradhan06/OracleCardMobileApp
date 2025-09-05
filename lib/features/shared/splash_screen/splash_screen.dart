0b5fd7742d9cb5b7f7033ec71c38c35a99567c18
nishan <nishanpradhan500@gmail.com>
2025-09-05 20:19:46 +0545

    splashscreen

--- lib/features/shared/splash_screen/splash_screen.dart
+++ lib/features/shared/splash_screen/splash_screen.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:oracle_card_app/router/app_routes_names.dart';
+import 'package:oracle_card_app/common/extension/extension.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        context.go(AppRoutesName.bottomNavBar.path);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
