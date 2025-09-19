9cb0d4cb0e7c344b9ce34abcdfe0661dea0083a5
nishan <nishanpradhan500@gmail.com>
2025-09-19 20:33:06 +0545

    loginscreen completed

---
+++ lib/core/helpers/textfield_validation_helpers.dart
@@ -0,0 +1,66 @@
+class InputValidator {
+  static String? validateName(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'Enter your name';
+    } else if (value.trim().length < 3) {
+      return 'Name must be at least 3 characters';
+    }
+    return null;
+  }
+
+  static String? validatePhone(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'Enter your phone number';
+    } else if (!RegExp(r'^\d{10}$').hasMatch(value)) {
+      return 'Phone number must be exactly 10 digits';
+    }
+    return null;
+  }
+
+  static String? validateEmail(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'Enter your email';
+    } else if (!RegExp(
+      r'^[a-zA-Z0-9.a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+    ).hasMatch(value)) {
+      return 'Enter a valid email address';
+    }
+    return null;
+  }
+
+  static String? validatePassword(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'Enter your password';
+    } else if (value.length < 6) {
+      return 'Password must be at least 6 characters long';
+    }
+    return null;
+  }
+
+  static String? validateConfirmPassword(
+    String? value,
+    String? originalPassword,
+  ) {
+    if (value == null || value.trim().isEmpty) {
+      return 'Confirm your password';
+    } else if (value != originalPassword) {
+      return 'Passwords do not match';
+    }
+    return null;
+  }
+
+  static String? validateEmailOrPhone(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'Enter your email or phone number';
+    }
+
+    final isEmail = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+    final isPhone = RegExp(r'^\d{10}$');
+
+    if (!isEmail.hasMatch(value.trim()) && !isPhone.hasMatch(value.trim())) {
+      return 'Enter a valid email or 10-digit phone number';
+    }
+
+    return null;
+  }
+}
--- lib/features/auth/screen/login_screen.dart
+++ lib/features/auth/screen/login_screen.dart
@@ -1,44 +1,83 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:oracle_card_app/core/helpers/textfield_validation_helpers.dart';
 import 'package:oracle_card_app/core/widgets/custom_background.dart';
 import 'package:oracle_card_app/core/widgets/custom_button.dart';
 import 'package:oracle_card_app/core/widgets/custom_container.dart';
+import 'package:oracle_card_app/core/widgets/custom_padding.dart';
+import 'package:oracle_card_app/features/auth/widgets/text_form_field.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomBackground(
-        child: CustomContainer(
-          child: Column(
-            children: [
-              Text(
-                'Welcome Back',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Username or Email',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
+        child: CustomPadding(
+          child: Center(
+            child: CustomContainer(
+              height: MediaQuery.sizeOf(context).height * 0.45,
+              child: Form(
+                key: _formKey,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 10,
+                    children: [
+                      Text(
+                        'Welcome Back',
+                        style: Theme.of(context).textTheme.headlineMedium
+                            ?.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF6B46C1),
+                            ),
+                      ),
+
+                      CustomTextField(
+                        label: 'Email',
+                        type: CustomTextFieldType.email,
+                        validator: InputValidator.validateEmail,
+                      ),
+                      CustomTextField(
+                        label: 'Password',
+                        obscureText: true,
+                        type: CustomTextFieldType.password,
+                        validator: InputValidator.validatePassword,
+                      ),
+
+                      CustomButton(
+                        width: double.infinity,
+                        text: 'Login',
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            log(_emailController.text);
+                            log(_passwordController.text);
+                          }
+                        },
+                      ),
+                    ],
                   ),
                 ),
               ),
-              const SizedBox(height: 20),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              CustomButton(text: 'Login', onPressed: () {}),
-            ],
+            ),
           ),
         ),
       ),
