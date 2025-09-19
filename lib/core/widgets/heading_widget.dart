56cd6eaeecdd35b79240277e619893fa26eb83ea
nishan <nishanpradhan500@gmail.com>
2025-09-19 13:38:22 +0545

    session screen

--- lib/core/widgets/heading_widget.dart
+++ lib/core/widgets/heading_widget.dart
@@ -8,6 +8,8 @@ class HeadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+
       children: [
         Text(
           title,
--- lib/features/sessions/screen/sessions_screen.dart
+++ lib/features/sessions/screen/sessions_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:oracle_card_app/core/widgets/custom_appbar.dart';
 import 'package:oracle_card_app/core/widgets/custom_background.dart';
+import 'package:oracle_card_app/core/widgets/custom_padding.dart';
+import 'package:oracle_card_app/core/widgets/heading_widget.dart';
+import 'package:oracle_card_app/core/widgets/upgrade_premium_button_widget.dart';
+
+import '../../../core/widgets/custom_chip.dart';
+import '../../home/widgets/notification_widget.dart';
 
 class SessionScreen extends StatelessWidget {
   const SessionScreen({super.key});
@@ -8,12 +14,28 @@ class SessionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(title: 'Sessions'),
+      appBar: CustomAppBar(
+        title: 'Sessions',
+        titleAlignment: TitleAlignment.left,
+        actions: [
+          NotificationIcon(hasNotification: true),
+          CustomChip(label: 'Premium', type: ChipType.premium),
+        ],
+      ),
       body: CustomBackground(
-        child: const Center(
-          child: Text(
-            'Sessions Screen',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        child: SingleChildScrollView(
+          child: CustomPadding(
+            child: Column(
+              spacing: 10,
+              children: [
+                HeadingWidget(
+                  title: 'Group Sessions',
+                  subtitle:
+                      'Join live guided sessions with our spiritual community.',
+                ),
+                UpgradePremiumButtonWidget(),
+              ],
+            ),
           ),
         ),
       ),
