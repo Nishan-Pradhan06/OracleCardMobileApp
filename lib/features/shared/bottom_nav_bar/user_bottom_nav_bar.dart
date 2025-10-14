5818c462f52551de12c7cee44378aa7f539db25a
nishan <nishanpradhan500@gmail.com>
2025-10-14 20:42:23 +0545

    double back tap button

--- lib/features/shared/bottom_nav_bar/user_bottom_nav_bar.dart
+++ lib/features/shared/bottom_nav_bar/user_bottom_nav_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import '../../../core/widgets/double_press_to_exit.dart';
 import '../../users/home/screen/home_screen.dart';
 import '../../users/journal/screen/journal_screen.dart';
 import '../../users/library/screen/library_screen.dart';
@@ -26,47 +27,49 @@ class _UserBottomNavBarScreenState extends State<UserBottomNavBarScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: IndexedStack(index: _currentIndex, children: _screens),
-      bottomNavigationBar: Material(
-        elevation: 8,
-        child: BottomNavigationBar(
-          backgroundColor: Color(0xFFFFFFFF),
-          type: BottomNavigationBarType.fixed,
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          items: [
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset('assets/svg/home.svg'),
-              activeIcon: SvgPicture.asset('assets/svg/home_bold.svg'),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset('assets/svg/journal.svg'),
-              activeIcon: SvgPicture.asset('assets/svg/journal_bold.svg'),
-              label: 'Journal',
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset('assets/svg/library.svg'),
-              activeIcon: SvgPicture.asset('assets/svg/library_bold.svg'),
-              label: 'Library',
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset('assets/svg/session.svg'),
-              activeIcon: SvgPicture.asset('assets/svg/session_bold.svg'),
-              label: 'Session',
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset('assets/svg/profile.svg'),
-              activeIcon: SvgPicture.asset('assets/svg/profile_bold.svg'),
-              label: 'Profile',
-            ),
-          ],
+    return DoubleBackToExit(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: IndexedStack(index: _currentIndex, children: _screens),
+        bottomNavigationBar: Material(
+          elevation: 8,
+          child: BottomNavigationBar(
+            backgroundColor: Color(0xFFFFFFFF),
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset('assets/svg/home.svg'),
+                activeIcon: SvgPicture.asset('assets/svg/home_bold.svg'),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset('assets/svg/journal.svg'),
+                activeIcon: SvgPicture.asset('assets/svg/journal_bold.svg'),
+                label: 'Journal',
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset('assets/svg/library.svg'),
+                activeIcon: SvgPicture.asset('assets/svg/library_bold.svg'),
+                label: 'Library',
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset('assets/svg/session.svg'),
+                activeIcon: SvgPicture.asset('assets/svg/session_bold.svg'),
+                label: 'Session',
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset('assets/svg/profile.svg'),
+                activeIcon: SvgPicture.asset('assets/svg/profile_bold.svg'),
+                label: 'Profile',
+              ),
+            ],
+          ),
         ),
       ),
     );
