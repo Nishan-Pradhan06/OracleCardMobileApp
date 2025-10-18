40915315d5d1a56169344e2da2551f249a750672
nishan <nishanpradhan500@gmail.com>
2025-10-18 16:52:25 +0545

    create model

--- lib/features/users/sessions/models/session_model.dart
+++ lib/features/users/sessions/models/session_model.dart
@@ -46,3 +46,83 @@ class Session {
     );
   }
 }
+
+class UpcomingSessionsDataModel {
+  final List<SessionItemModel> items;
+
+  UpcomingSessionsDataModel({required this.items});
+
+  factory UpcomingSessionsDataModel.fromJson(Map<String, dynamic> json) {
+    return UpcomingSessionsDataModel(
+      items:
+          (json['items'] as List<dynamic>?)
+              ?.map((e) => SessionItemModel.fromJson(e))
+              .toList() ??
+          [],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'items': items.map((e) => e.toJson()).toList()};
+  }
+}
+
+class SessionItemModel {
+  final int id;
+  final String title;
+  final String description;
+  final DateTime startTime;
+  final DateTime endTime;
+  final String timezone;
+  final String zoomUrl;
+  final bool isPremiumOnly;
+  final int createdById;
+  final bool rsvp;
+  final bool canJoin;
+
+  SessionItemModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.startTime,
+    required this.endTime,
+    required this.timezone,
+    required this.zoomUrl,
+    required this.isPremiumOnly,
+    required this.createdById,
+    required this.rsvp,
+    required this.canJoin,
+  });
+
+  factory SessionItemModel.fromJson(Map<String, dynamic> json) {
+    return SessionItemModel(
+      id: json['id'] ?? 0,
+      title: json['title'] ?? '',
+      description: json['description'] ?? '',
+      startTime: DateTime.tryParse(json['startTime'] ?? '') ?? DateTime.now(),
+      endTime: DateTime.tryParse(json['endTime'] ?? '') ?? DateTime.now(),
+      timezone: json['timezone'] ?? '',
+      zoomUrl: json['zoomUrl'] ?? '',
+      isPremiumOnly: json['isPremiumOnly'] ?? false,
+      createdById: json['createdById'] ?? 0,
+      rsvp: json['rsvp'] ?? false,
+      canJoin: json['canJoin'] ?? false,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'startTime': startTime.toIso8601String(),
+      'endTime': endTime.toIso8601String(),
+      'timezone': timezone,
+      'zoomUrl': zoomUrl,
+      'isPremiumOnly': isPremiumOnly,
+      'createdById': createdById,
+      'rsvp': rsvp,
+      'canJoin': canJoin,
+    };
+  }
+}
