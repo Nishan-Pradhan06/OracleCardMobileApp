08dec2d04b65e3f98958933464fef15f879e8664
nishan <nishanpradhan500@gmail.com>
2025-10-18 16:58:58 +0545

    create repo for session

---
+++ lib/features/users/sessions/repository/session_repository.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:oracle_card_app/common/typedef/either_type.dart';
+import 'package:oracle_card_app/core/network/api_services.dart';
+import 'package:oracle_card_app/features/users/sessions/models/session_model.dart';
+
+abstract interface class SessionRepository {
+  FutureEither<UpcomingSessionsDataModel> getUpcommingSession();
+}
+
+class SessionRepositoryImpl implements SessionRepository {
+  final ApiService _apiService;
+
+  SessionRepositoryImpl({required ApiService apiServices})
+    : _apiService = apiServices;
+
+  @override
+  FutureEither<UpcomingSessionsDataModel> getUpcommingSession() async {
+    final response = await _apiService.get('sessions');
+
+    return response.fold((failure) => Left((failure)), (data) {
+      final dataJson = data['data'];
+      final getUpCommingSessions = UpcomingSessionsDataModel.fromJson(dataJson);
+      return Right(getUpCommingSessions);
+    });
+  }
+}
