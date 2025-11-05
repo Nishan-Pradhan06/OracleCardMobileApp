1e13db1166617f1b8a039a80e24a0208adee4c10
nishan <nishanpradhan500@gmail.com>
2025-11-05 21:15:46 +0545

    repo for deck

--- lib/features/admin/repository/admin_repository.dart
+++ lib/features/admin/repository/admin_repository.dart
@@ -1,6 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:oracle_card_app/common/typedef/either_type.dart';
 import 'package:oracle_card_app/core/network/api_services.dart';
+import 'package:oracle_card_app/features/admin/deck_and_card/model/deck_model.dart';
 
-abstract interface class AdminRepository {}
+abstract interface class AdminRepository {
+  //##-------------------CREATE DECK-------------------------##
+  FutureEither<String> createDeck({required DeckCardModel deckCardModel});
+}
 
 class AdminRepositoryImp implements AdminRepository {
   final ApiService _apiService;
@@ -8,5 +14,17 @@ class AdminRepositoryImp implements AdminRepository {
   AdminRepositoryImp({required ApiService apiService})
     : _apiService = apiService;
 
-    
+  @override
+  FutureEither<String> createDeck({
+    required DeckCardModel deckCardModel,
+  }) async {
+    final response = await _apiService.post<Map>(
+      'admin/decks',
+      data: {...deckCardModel.toMap()},
+    );
+
+    return response.fold((failure) => Left(failure), (data) {
+      return Right("Deck Create Successful !!!");
+    });
+  }
 }
